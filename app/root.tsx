6c8088d599d1d2087b0b216478b641571511e3e4
tim <tim@zolleis.net>
2023-11-25 20:59:02 +0100

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,38 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import {cssBundleHref} from "@remix-run/css-bundle";
+import type {LinksFunction} from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
 
+import tailwindStyles from "./tailwind.css"
+
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+    {rel: "stylesheet", href: tailwindStyles},
+    {rel: "preconnect", href: "href=https://rsms.me/"},
+    {rel: "stylesheet", href: "https://rsms.me/inter/inter.css"},
 ];
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body className={"font-inter"}>
+        <Outlet/>
+        <ScrollRestoration/>
+        <Scripts/>
+        <LiveReload/>
+        </body>
+        </html>
+    );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,34 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {MetaFunction} from "@remix-run/node";
+import {Link} from "@remix-run/react";
+import {Button} from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        {title: "New Remix App"},
+        {name: "description", content: "Welcome to Remix!"},
+    ];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+            <div className="container px-4 md:px-6">
+                <div className="flex flex-col items-center space-y-4 text-center">
+                    <div className="space-y-2">
+                        <h1 className="text-3xl font-bold font-inter tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                            Generate random data. Fast.
+                        </h1>
+                        <p className="mx-auto max-w-[700px] text-zinc-500 md:text-xl dark:text-zinc-400">
+                            Generate vast amounts of random data in a variety of formats using a beautiful and intuitive
+                            interface.
+                        </p>
+                    </div>
+                    <Button className={"rounded-full"}
+                    >
+                       Start generating
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
 }
